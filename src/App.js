9bdc7d21374db7747cb8b7a05534cab6463e2aca
Ryan Frost-Garant <rfros048@uottawa.ca>
2023-06-27 17:51:49 -0400

    fix2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import TopNavbar from './components/TopNavbar';
 import AboutUs from './components/pages/AboutUs';
 import CreateRequest from './components/pages/CreateRequest';
