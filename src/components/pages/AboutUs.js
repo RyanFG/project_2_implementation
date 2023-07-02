5dc5aa529f69cd8b76707c18ad5b7406a7b31c8e
Ryan Frost-Garant <rfros048@uottawa.ca>
2023-07-02 16:40:50 -0400

    added to the tips page

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -38,11 +38,11 @@ function AboutUs(){
                             Send Us Any Questions
                         </h1>
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
+                            <Form.Label>Email</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                            <Form.Label>Comment</Form.Label>
+                            <Form.Label>Comments</Form.Label>
                             <Form.Control as="textarea" rows={3} />
                         </Form.Group>
                         <Button variant="success" type="submit">
